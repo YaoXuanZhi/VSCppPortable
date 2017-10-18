2e1372bca5589956990695d9b7d2e4995c7a6196
yaoxuanzhi <yaoxuanzhi@outlook.com>
2017-10-18 23:26:57 +0800

    添加RunCode.bat

---
+++ RunCode.bat
@@ -0,0 +1,11 @@
+﻿:::::::::::::::: Visual Studio Code Portable Launcher Version 1.0 BY 耀轩之 ::::::::::::::::
+@echo off
+call:set_absolute_path USERPROFILE .\RunDir\User
+call:set_absolute_path APPDATA .\RunDir\User\AppData\Roaming
+call:set_absolute_path CompilerPath .\RunDir\Compiler
+start Code.exe
+exit
+
+:set_absolute_path
+for /f %%p in ("%2") do (set %1=%%~fp)
+goto:eof
