c379e7d153d8cac913bf34943b75dbdd153e463a
yaoxuanzhi <yaoxuanzhi@outlook.com>
2017-10-21 01:43:35 +0800

    更新RunCodeEx.bat

--- RunCodeEx.bat
+++ RunCodeEx.bat
@@ -4,4 +4,11 @@ set USERPROFILE=%~dp0RunDir\User
 set APPDATA=%~dp0RunDir\User\AppData\Roaming
 set CompilerPath=%~dp0RunDir\Compiler
 
-"%~dp0Code.exe" %1
+::进入当前bat的所在盘符
+%~d0
+
+::进入当前bat的所在目录
+cd %~dp0
+
+::传入外部参数并执行Code.exe
+start Code.exe %1
