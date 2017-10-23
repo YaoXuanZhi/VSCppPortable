a77b231e76977b18a7b015f41c230a561d8f39cd
yaoxuanzhi <yaoxuanzhi@outlook.com>
2017-10-23 22:19:19 +0800

    添加软链接支持（Vista及其以上系统才支持）

---
+++ MdLink.bat
@@ -0,0 +1,6 @@
+::判断是否已经获取了管理员身份
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行&&Pause >NUL&&Exit)
+Rd "%WinDir%\System32\test_permissions" 2>NUL
+SetLocal EnableDelayedExpansion
+
+mklink /j "%USERPROFILE%\.vscode" "%~dp0RunDir\User\.vscode"
