b7dadcd9700b0485455d6ac072fcda3549699887
yaoxuanzhi <yaoxuanzhi@outlook.com>
2017-10-23 22:55:50 +0800

    更新RegCode.bat

--- RegCode.bat
+++ RegCode.bat
@@ -11,19 +11,29 @@ SetLocal EnableDelayedExpansion
 @set runbat="RunCode.bat"
 ::设置子路径名称
 @set subname="VSCode"
+::设置菜单项图标
+@set iconpath="%~dp0Code.exe"
 ::去掉双引号，主要是为了兼容包含了空格的文件名或路径
 @set "runbat=%runbat:"=%"
 @set "subname=%subname:"=%"
 
 ::添加注册表信息
-@reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "" /t REG_SZ /d %desc%
-@reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%~dp0Code.exe"
-@reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%\command" /v ""  /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+@reg add "HKEY_CLASSES_ROOT\*\shell\%subname%" /v "" /t REG_SZ /d %desc%
+@reg add "HKEY_CLASSES_ROOT\*\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d %iconpath%
+@reg add "HKEY_CLASSES_ROOT\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
 
-@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "" /t REG_SZ /d %desc%
-@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%~dp0Code.exe"
-@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+@reg add "HKEY_CLASSES_ROOT\Drive\shell\%subname%" /v "" /t REG_SZ /d %desc%
+@reg add "HKEY_CLASSES_ROOT\Drive\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d %iconpath%
+@reg add "HKEY_CLASSES_ROOT\Drive\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
 
 @reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "" /t REG_SZ /d %desc%
-@reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%~dp0Code.exe"
+@reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d %iconpath%
 @reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+
+@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "" /t REG_SZ /d %desc%
+@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d %iconpath%
+@reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%subname%" /v "" /t REG_SZ /d %desc%
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d %iconpath%
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\%subname%\command" /v ""  /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
